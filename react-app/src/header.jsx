0a8c8a16a3ed4189d22887bbd306e8c7f438b35b
Daniel-Oshione <omobsdaniel@gmail.com>
2024-09-24 10:05:36 +0100

    learning

---
+++ react-app/src/header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+return(
+    <header>
+        <nav>
+            Header
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+}
+
+export default Header
